e607b28d9482dbcfba51d0d423bd8ff3726319cb
Chaitenyakumar <63350449+Chaitenyakumar@users.noreply.github.com>
2025-09-07 10:49:47 +0530

    intro seciond file

---
+++ Intro_to_python/data_type_intro_02.py
@@ -0,0 +1,28 @@
+# # 1. Data-types:
+#
+# str         # - when anything is enclosed with ' ' or " " or ''' ''' or """ """
+# int         # - non-decimal number from
+# float       # - decimal number from
+# bool        # - True or False
+# list        # - [1,2,3]
+# tuple       # - (1,2,3)
+# set         # - {1,2,3}
+# dict        # - {key1:value1 , key2:value2}
+# NONE
+
+
+print(3.14)
+print(type(2.07))
+
+
+# Boolean case sensitive
+print(True)
+print(False)
+print(type(False))
+
+#this is not boolean
+print('true')  # this is a string
+print(type('true'))  # this is a string
+
+
+
