e7d1ba537b88457b5faaaaf46f292d7e775ddd87
Chaitenyakumar <63350449+Chaitenyakumar@users.noreply.github.com>
2025-09-20 09:51:52 +0530

    Add examples for assignment operators in Python

---
+++ 03_numbers_operators/numbers_07.py
@@ -0,0 +1,37 @@
+# Assignment Operators in Python
+# Assignment operators are used to assign values to variables and perform operations in-place.
+
+# Basic assignment (=)
+num = 10
+print("= operator:", num)  # Output: 10
+
+# Add and assign (+=)
+num += 5  # Equivalent to num = num + 5
+print("+= operator:", num)  # Output: 15
+
+# Subtract and assign (-=)
+num -= 3  # Equivalent to num = num - 3
+print("-= operator:", num)  # Output: 12
+
+# Multiply and assign (*=)
+num *= 2  # Equivalent to num = num * 2
+print("*= operator:", num)  # Output: 24
+
+# Divide and assign (/=)
+num /= 4  # Equivalent to num = num / 4
+print("/= operator:", num)  # Output: 6.0
+
+# Floor divide and assign (//=)
+num //= 2  # Equivalent to num = num // 2
+print("//= operator:", num)  # Output: 3.0
+
+# Modulus and assign (%=)
+num %= 2  # Equivalent to num = num % 2
+print("%= operator:", num)  # Output: 1.0
+
+# Exponent and assign (**=)
+num **= 3  # Equivalent to num = num ** 3
+print("**= operator:", num)  # Output: 1.0
+
+# Assignment operators are useful for updating variable values efficiently.
+
