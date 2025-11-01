530219e70fba35f0b7518af31b27d18c8d31a12a
Chaitenyakumar <63350449+Chaitenyakumar@users.noreply.github.com>
2025-11-01 10:27:00 +0530

    oops

---
+++ 12_OOPS/OOPS_1.py
@@ -0,0 +1,24 @@
+
+
+"""
+Object oriented programming
+
+is a method of programming that attempts to model some process 
+or thing in the world as a class or object.
+
+An object is any entity that has attributes and behaviors. 
+For example, a parrot is an object. It has
+
+attributes - name, age, color, etc.
+behavior - dancing, singing, etc.
+
+class - a blueprint for objects.
+ Classes can contain methods (functions) and attributes (similar to keys in a dict).
+ 
+"""
+
+"""
+instance - objects that are constructed from a class 
+blueprint that contain their class's methods and properties.
+"""
+
